20ad1c975afab2606d92108a60f9583ea70dfef8
EasySouls <retek@DESKTOP-HTNKTMG>
2022-12-30 15:54:58 +0100

    homepage done

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,11 @@ import Navbar from "./Navbar";
 export default function Header() {
   return (
     <>
-      <div className='h-[15%] bg-[url("./assets/trees.jpg")] bg-cover' />
+      <div className='h-[15%] w-full bg-[url("./assets/golden-forest.jpg")] bg-cover flex items-center justify-center'>
+        <div className="bg-black rounded-full h-1/2 aspect-square flex items-center">
+          <img src="src/assets/stellar.png" alt="logo" className="h-3/5 m-auto"/>
+        </div>
+      </div>
       <Navbar />
     </>
   );
