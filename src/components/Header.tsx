700b531756a41d1de44affc21d100a4e64783418
Csanád Tarjányi <tarjanyicsanad@gmail.com>
2023-01-03 14:35:27 +0100

    changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,18 +3,25 @@ import Navbar from "./Navbar";
 import { useNavigate } from "react-router-dom";
 
 export default function Header() {
-  const navigate = useNavigate()
-  
+  const navigate = useNavigate();
+
   function returnToHomepage(e: React.MouseEvent) {
-    e.preventDefault()
-    navigate('/')
+    e.preventDefault();
+    navigate("/");
   }
 
   return (
     <>
       <div className='h-[15%] w-full bg-[url("./assets/golden-forest.jpg")] bg-cover flex items-center justify-center'>
-        <button className="bg-black rounded-full h-1/2 aspect-square flex items-center" onClick={returnToHomepage}>
-          <img src="/stellar.png" alt="logo" className="h-3/5 m-auto"/>
+        <button
+          className='bg-black rounded-full h-1/2 aspect-square flex items-center'
+          onClick={returnToHomepage}
+        >
+          <img
+            src='src/assets/stellar.png'
+            alt='logo'
+            className='h-3/5 m-auto'
+          />
         </button>
       </div>
       <Navbar />
